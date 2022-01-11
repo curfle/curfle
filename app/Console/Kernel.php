471af20db16c362c7f75c7cd6a32f0451c1038e1
Julius Türich <info@joonlabs.com>
2022-01-11 23:45:54 +0100

    add support for scheduling tasks in console kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,10 +2,22 @@
 
 namespace App\Console;
 
+use Curfle\Console\Schedule;
+use Curfle\Essence\Application;
 use Curfle\Essence\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    /**
+     * Register events, commands and functions that shall be executed in the future.
+     * 
+     * @param Schedule $schedule
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command("inspire")->everyMinute();
+    }
+
     /**
      * Register the commands for the application.
      *
@@ -14,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->loadFromDirectory(__DIR__ . "/Commands");
+
         require base_path('routes/console.php');
     }
 }
